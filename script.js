a23b432a03afe6bf51dec5a7b504a940a2a8efc7
ChandanaEkanayaka <jagath99t@gmail.com>
2024-06-02 11:42:16 +0530

    done

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const signUpButton=document.getElementById('signUpButton');
 const signInButton=document.getElementById('signInButton');
 const signInForm=document.getElementById('signIn');
-const signUpForm=document.getElementById('signUp');
-
+const signUpForm=document.getElementById('signup');
 
 signUpButton.addEventListener('click',function(){
     signInForm.style.display="none";
     signUpForm.style.display="block";
-});
-
-signInButton.addEventListener('click',function(){
+})
+signInButton.addEventListener('click', function(){
     signInForm.style.display="block";
     signUpForm.style.display="none";
-});
+})
